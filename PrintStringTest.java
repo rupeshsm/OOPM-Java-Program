05003b2b5f8eff9173650b2748970acd5289d5b2
rupeshsm <rrupesh.mishra@gmail.com>
2018-07-23 10:09:04 +0530

    1. To Display user defined text using non static method.
    Non static method
    1. Default method type
    2. Accessed by object of its class
    
    In this program displayText() method is accessed by the object of class PrintString in the main function of class PrintStringTest.

---
+++ PrintStringTest.java
@@ -0,0 +1,22 @@
+/** Object Oriented Program to Display Text Message **/
+class PrintString
+{
+	
+	void displayText()
+	{
+		System.out.println("Hello, World :) ");
+	}
+}
+
+/*** Main Class***/
+
+class PrintStringTest
+{
+	public static void main(String args[])
+	{
+		
+		PrintString p = new PrintString();
+		p.displayText();
+		
+	} 
+}
