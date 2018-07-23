ba4c8d8add46242f926140b4a003c22cb5486dc5
rupeshsm <rrupesh.mishra@gmail.com>
2018-07-23 09:58:29 +0530

    1. Object Oriented Program to display text message with non static method.
    This program will display user defined text on console screen using non static method

---
+++ TestJava.java
@@ -0,0 +1,21 @@
+class PrintString
+{
+	
+	void displayText()
+	{
+		System.out.println("Hello, World :) ");
+	}
+}
+
+/*** Main Class***/
+
+class PrintStringTest
+{
+	public static void main(String args[])
+	{
+		
+		PrintString p = new PrintString();
+		p.displayText();
+		
+	} 
+}
