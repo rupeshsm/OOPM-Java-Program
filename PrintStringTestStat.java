c8ad276d7c6bc875f10e2a6898267fc8c0acb4c1
rupeshsm <rrupesh.mishra@gmail.com>
2018-07-23 10:12:28 +0530

    2. To display text message from static method
    Static Method 
    1. Variable of class
    2. Only one copy is shared with every object.
    3. Accessed directly from class

---
+++ PrintStringTestStat.java
@@ -0,0 +1,21 @@
+/** Object Oriented Program to Display Text Message **/
+class PrintString
+{
+	
+	static void displayText()
+	{
+		System.out.println("Hello, World :) ");
+	}
+}
+
+/*** Main Class***/
+
+class PrintStringTestStat
+{
+	public static void main(String args[])
+	{
+		
+		PrintString.displayText();
+		
+	} 
+}
