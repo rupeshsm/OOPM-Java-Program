945c2c6e5f98b36852fc70b75dbec3882e2b8460
Rupesh Mishra <rrupesh.mishra@gmail.com>
2018-07-26 13:59:00 +0530

    6. Method with return value
    In this program simple method overtime() is created to calculate extra hours worked by the employee and returned the extra work time.

---
+++ TestEmployeeOvertime.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+class Employee
+{
+	String name;
+	int e_id;
+	float salary;
+	boolean status;
+	float work_hrs;
+	Scanner sc = new Scanner(System.in);
+	void setData()
+	{
+		
+		System.out.println("Enter Name (String) of Employee: " );
+		name=sc.nextLine();
+		System.out.println("Enter Employee ID (Integer) of Employee: " );
+		e_id=sc.nextInt();
+		System.out.println("Enter Working Hours(Float) of Employee: " );
+		work_hrs=sc.nextFloat();
+		System.out.println("Enter Salary (Float) of Employee: " );
+		salary=sc.nextFloat();
+		System.out.println("Enter Working Satus (Boolean) of Employee: " );
+		status=sc.nextBoolean();
+		
+	}
+	void getData()
+	{
+		System.out.println("Name:"+name+"\nEmp Id:"+e_id+"\nSalary:"+salary+"\nStatus:"+status);
+	}
+	float overtime()
+	{
+		float ot = work_hrs-40;
+		return ot; 
+	}
+}
+
+/*** Main Class***/
+
+class TestEmployeeOvertime
+{
+	public static void main(String args[])
+	{
+		
+		Employee e = new Employee();
+		e.setData();
+		e.getData();
+		float ot = e.overtime();
+		if(ot > 0)
+		{
+			System.out.println(e.name +" worked "+ot+" Hrs Extra");
+		}
+		else
+		{
+			System.out.println(e.name +" Not qualified for extra pay");
+		}
+		
+	} 
+}
